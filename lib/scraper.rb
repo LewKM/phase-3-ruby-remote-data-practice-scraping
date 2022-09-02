7dfcd33def27cf7489df7be6066eebe6bb114803
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-02 14:53:10 +0300

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+
+html = open("https://flatironschool.com/our-courses/")
+# Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(html)
+puts doc.css(".heading-35-semibold.color-red.mb-20").text.strip
+
+
+courses = doc.css(".heading-60-black.color-black.mb-20")
+courses.each do |course|
+    puts course.text.strip
+end
